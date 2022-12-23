ef50a7017773efd7c0655b7dc8aeb81c61dbd23d
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-12-23 15:40:44 +0100

    style fix

--- resources/views/Gifts/show.blade.php
+++ resources/views/Gifts/show.blade.php
@@ -4,17 +4,17 @@
 @endsection
 
 @section('content')
-<section class="section-main outer-container-cards flex-column">
+<section class="section-main outer-container-cards  flex-column flex-wrap">
    
-    <div class="bg-black h-50 w-50 d-flex">
+    <div class="bg-black w-75 d-flex">
         {{-- img --}}
-        <div class="col-4 bg-white">
-            <img src="{{$gift->image}}" alt="">
+        <div class="col-4 bg-white d-flex align-items-center align-content-center">
+            <div><img src="{{$gift->image}}" alt=""></div>
         </div>
         {{-- testo --}}
-        <div class="col-8 single-card-text">
+        <div class="col-6 single-card-text flex-column">
             {{-- dettagli --}}
-            <div class="flex-grow-1">
+            <div class="flex-grow-1  ps-4">
                 <p class="text-white">{{$gift->gift}}</p>
                 <p class="text-white">{{$gift->name}}</p>
                 <p class="text-white">{{$gift->surname}}</p>
@@ -23,17 +23,19 @@
                 <p class="text-white">{{$gift->city}}</p>
                 <p class="text-white">{{$gift->address}}</p>
             </div>
-            {{-- btns --}}
-            <div class=" d-flex gap-2 pb-3">
+            
+        </div>
+        <div class="col-2 d-flex flex-column pb-3">
+            <div class="text-center">
                 <a href={{route('gifts.edit', $gift->id)}}>
-                <button>modifica</button>
+                modifica
                 </a>
-                <form action={{route('gifts.destroy', $gift->id)}} method="POST">
-                @csrf
-                @method('DELETE')
-                <button class="noselect delete"><span class='text'>Delete</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span></button>
-                </form>
             </div>
+            <form action={{route('gifts.destroy', $gift->id)}} method="POST">
+            @csrf
+            @method('DELETE')
+            <button class="noselect delete"><span class='text'>Delete</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span></button>
+            </form>
         </div>
       
     </div>
